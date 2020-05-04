46d972f6a6f2315e7901101868c811b77add73be
Ajani Bilby <me@ajanibilby.com>
2020-05-04 20:51:38 +1000

    ~Fixed bad relative file path

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ let Compile = require('./src/compiler.js');
 let Parse = require('./src/parser.js');
 let types = require('./src/types.js');
 
+const path = require('path');
 const fs = require('fs');
 
+let file = path.join(__dirname, './bnf.json');
+
 let syntax = types.BNF_Tree.fromJSON(
-  JSON.parse(fs.readFileSync('./bnf.json', 'utf8'))
+  JSON.parse(fs.readFileSync(file, 'utf8'))
 );
 
 module.exports = {Compile, Parse, types, syntax};
