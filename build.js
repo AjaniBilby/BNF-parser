0890cfe4fcd8e307907dedec00f1ebb27b0b8400
Ajani Bilby <me@ajanibilby.com>
2020-05-04 20:27:31 +1000

    ~Updated build script

--- build.js
+++ build.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
-let data = fs.readFileSync('./bnf.bnf', 'utf8');
-let tree = JSON.parse(fs.readFileSync('./bnf.json', 'utf8'));
-
 let BNF = require('./index.js');
 
-let res = BNF.Parse(data, tree, "program");
-fs.writeFileSync('temp-tree.json', JSON.stringify(res, null, 2));
 
+// Load the BNF file
+const fs = require('fs');
+let data = fs.readFileSync('./bnf.bnf', 'utf8');
+
+// Parse the file and check for errors
+let res = BNF.Parse(data, BNF.syntax);
 if (res.hasError || res.isPartial) {
 	console.error("BNF didn't parse correctly");
 	process.exit(1);
 }
 
-
+// Compile the parsed result into a new tree
 let syntax = BNF.Compile(res.tree);
 fs.writeFileSync('bnf.json', JSON.stringify(syntax, null, 2));
 
-
+// Print success
 console.log('BNF syntax building completed');
 process.exit(0);
