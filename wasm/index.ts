a5b4182d2126f276648587526e6bd08077c5ad90
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-06-26 17:41:52 +1000

    +Basic string matching

--- wasm/index.ts
+++ wasm/index.ts
@@ -5,24 +5,41 @@ import * as path from "path";
 type WasmParser = WebAssembly.Instance & {
 	exports: {
 		memory      : WebAssembly.Memory;
+		input       : WebAssembly.Global;
 		inputLength : WebAssembly.Global;
-		heap        : WebAssembly.Global;
 
-		add: (a: number, b: number) => number;
-		parse: (index: number) => void;
+		_init: () => void;
+		program: (index: number) => void;
 	}
 }
 
 
 async function Init(wasm: BufferSource){
-	const bundle = await WebAssembly.instantiate(wasm);
+	const bundle = await WebAssembly.instantiate(wasm, {
+		js: {
+			print_i32: console.log,
+			print_i32_i32_i32: console.log
+		}
+	});
 	return bundle.instance as WasmParser;
 }
 
-function Parse(ctx: WasmParser, data: string) {
+function Parse(ctx: WasmParser, data: string, entry?: string) {
 	InjectString(ctx, data);
 
-	ctx.exports.parse(0);
+	ctx.exports._init();
+	console.log("init");
+
+	const startIndex = ctx.exports.input.value;
+
+	if (entry) {
+		const funcs = ctx.exports as any;
+		if (typeof(funcs[entry]) !== "function") throw new Error(`Unknown entry point ${entry}`);
+
+		funcs[entry](startIndex);
+	} else {
+		ctx.exports.program(startIndex);
+	}
 
 	return Decode(ctx);
 }
@@ -32,8 +49,9 @@ function Decode(ctx: WasmParser) {
 	const memory = ctx.exports.memory;
 	const memoryArray = new Int32Array(memory.buffer);
 
-	const address = ctx.exports.inputLength.value;
-	const value = memoryArray[address / Int32Array.BYTES_PER_ELEMENT];
+	const address = ctx.exports.input.value + ctx.exports.inputLength.value;
+	console.log(`reading ${address}`);
+	const value = memoryArray[Math.ceil(address / Int32Array.BYTES_PER_ELEMENT)];
 	console.log(`"b" at index ${value}`);
 }
 
@@ -41,19 +59,17 @@ function Decode(ctx: WasmParser) {
 
 function InjectString(ctx: WasmParser, data: string) {
 	const memory = ctx.exports.memory;
+	memory.grow(1); // grow memory if needed
 
 	// Convert the string to UTF-8 bytes
 	const utf8Encoder = new TextEncoder();
 	const stringBytes = utf8Encoder.encode(data);
 
-	// Allocate space in the WebAssembly memory for the string
-	const newMemory = new Uint8Array(memory.buffer.byteLength + stringBytes.byteLength * 20);
-	newMemory.set(new Uint8Array(memory.buffer));
-	memory.grow(1); // grow memory if needed
-
 	// Copy the string bytes to WebAssembly memory
 	const wasmMemory = new Uint8Array(memory.buffer);
-	wasmMemory.set(stringBytes, 0);
+	wasmMemory.set(stringBytes, ctx.exports.input.value);
+
+	console.log(66, wasmMemory);
 
 	ctx.exports.inputLength.value = stringBytes.byteLength;
 }
