712023ac4ade67e4541d5c3b064104e962278c6b
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-07-14 21:45:04 +1000

    ~Exposed helpful endpoints

--- source/index.ts
+++ source/index.ts
@@ -1,11 +1,14 @@
-import * as Shared from "./artifacts/shared.js";
 import * as legacy from "./legacy/index.js";
 import * as wasm from "./wasm/index.js";
 import * as types from "./types.js";
 
+import * as _shared from "../dist/shared.js";
+import * as bnf from "../dist/bnf.js";
+
 export {
 	wasm,
 	legacy,
-	Shared,
-	types
+	types,
+	_shared,
+	bnf
 };
