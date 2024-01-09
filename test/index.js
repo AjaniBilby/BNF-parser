b1533b00b7974e971eb628bd4c64a88fc97a49c2
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2024-01-09 17:41:20 +1100

    ~simplified testing framework

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
-import { execSync } from "node:child_process";
-import { pathToFileURL } from "node:url";
 import path from "node:path";
 import chalk from "chalk";
 import fs from "node:fs";
+import { pathToFileURL } from "node:url";
+import { execSync } from "node:child_process";
+import { readFile } from "node:fs/promises";
 
 const cwd = path.dirname(process.argv[1]);
 
@@ -23,109 +24,81 @@ function CompileBNFs(){
 
 
 
-async function SampleTests() {
+async function TestSyntaxes() {
 	const syntaxes = fs.readdirSync(`${cwd}/bnf`)
 		.filter(x => x.endsWith(".bnf"))
 		.map(x => x.slice(0, -4));
 
 	const Shared = await import(pathToFileURL(`${cwd}/bnf/shared.js`));
 
-
 	console.log("Automated Sample Tests");
-	let failure = false;
-	for (const lang of syntaxes) {
-		console.log(" ", lang);
-		const dir = `${cwd}/sample/${lang}`;
-		if (!fs.existsSync(dir)) continue;
-
-		let files = fs.readdirSync(dir);
-		if (files.length === 0) continue;
-		files = files.filter(x => x.endsWith(".txt"));
-
-		const syntaxURL = pathToFileURL(`${cwd}/bnf/${lang}.js`);
-		const Parser = await import(syntaxURL);
-
-		if (!Parser.Parse_Program) {
-			continue;
-		}
-
-		for (const file of files) {
-			const data = fs.readFileSync(`${dir}/${file}`, "utf8");
-			try {
-				const syntax = Parser.Parse_Program(data, true);
-				if (syntax instanceof Shared.ParseError) throw syntax;
-				if (syntax.isPartial) throw new Shared.ParseError(
-					"Partial Match",
-					new Shared.ReferenceRange(syntax.root.ref.end, syntax.reach)
-				).toString();;
-
-				console.log(`    ${chalk.green("PASS")} ${file}`);
-			} catch (e) {
-				console.error(`    ${chalk.red("FAIL")} ${file}`);
-				console.error(e);
-				console.error("");
-				failure = true;
-			}
-		}
-	}
-
-	if (failure) process.exit(1);
+	const results = await Promise.all(syntaxes.map(l => TestSyntax(l, Shared)))
+	if (results.some(x => x === false)) process.exit(1);
 }
 
+async function TestSyntax(lang, Shared) {
+	let log = ' ' + lang;
 
+	// See if a folder exists with samples
+	const dir = `${cwd}/sample/${lang}`;
+	if (!fs.existsSync(dir)) return true;
 
+	let files = fs.readdirSync(dir)
+		.filter(x => x.endsWith(".txt"));
 
+	// Don't bother loading the parser if there are no files to parse
+	if (files.length === 0) return true;
 
-function ManualTests () {
-	let tests = {
-		// "Double Parse":     ['node double-parse.js',    { cwd }],
-		// "Uniview Parse":    ['node ./uv/index.js',      { cwd }],
-	};
+	const syntaxURL = pathToFileURL(`${cwd}/bnf/${lang}.js`);
+	const Parser = await import(syntaxURL);
+	if (!Parser.Parse_Program) return false;
+	await Parser?.ready;
 
 	let failed = false;
+	for (const file of files) {
+		const data = await readFile(`${dir}/${file}`, "utf8");
 
-	for (let key in tests) {
-		let test = tests[key];
-		console.log(key);
-
-		test[1].stdio = [null, null, null];
-
-		let res;
 		try {
-			res = execSync(test[0], test[1]);
-			console.log("  Passed\n");
-		} catch (e) {
-			console.error("  "+
-				e.stderr
-					.toString()
-					.replace(/\n+/g, " \n")
-			);
+			const syntax = Parser.Parse_Program(data, true);
+			if (syntax instanceof Shared.ParseError) throw syntax;
+			if (syntax.isPartial) throw new Shared.ParseError(
+				"Partial Match",
+				new Shared.ReferenceRange(syntax.root.ref.end, syntax.reach)
+			).toString();
 
-			console.log('  Failed\n');
+			log += `\n    ${chalk.green("PASS")} ${file}`;
+		} catch (e) {
+			log += `\n    ${chalk.red("FAIL")} ${file}`;
+			log += `\n${e}\n`;
 			failed = true;
 		}
 	}
 
-	process.exit(failed ? 1 : 0);
+	if (failed) {
+		console.error(log);
+	} else {
+		console.log(log);
+	}
+
+	return failed;
 }
 
 
 async function Main() {
-	// // Symbolic link to actual bnf
-	// const testBnfSyntax = "./test/bnf/bnf.bnf";
-	// if (!fs.existsSync(testBnfSyntax)) {
-	// 	fs.linkSync("./bnf/bnf.bnf", testBnfSyntax);
-	// }
+	// Symbolic link to actual bnf
+	const testBnfSyntax = "./test/bnf/bnf.bnf";
+	if (!fs.existsSync(testBnfSyntax)) {
+		fs.linkSync("./bnf/bnf.bnf", testBnfSyntax);
+	}
 
-	// // Update sample since it's renamed
-	// const testBnfSample = "./test/sample/bnf/self.txt";
-	// if (fs.existsSync(testBnfSample)) {
-	// 	fs.unlinkSync(testBnfSample);
-	// }
-	// fs.linkSync("./bnf/bnf.bnf", testBnfSample);
+	// Update sample since it's renamed
+	const testBnfSample = "./test/sample/bnf/self.txt";
+	if (fs.existsSync(testBnfSample)) {
+		fs.unlinkSync(testBnfSample);
+	}
+	fs.linkSync("./bnf/bnf.bnf", testBnfSample);
 
 	CompileBNFs();
-	await SampleTests();
-	ManualTests();
+	await TestSyntaxes();
 }
 Main();
